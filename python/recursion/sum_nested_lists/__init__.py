bde20b63b30f34e73195cef657f5932de8a89452
anarcho <aaron.karslen@gmail.com>
2024-09-29 12:11:39 +1000

    moved the init py file to the right place

---
+++ python/recursion/sum_nested_lists/__init__.py
@@ -0,0 +1,15 @@
+def sum_nested_list(nested_list):
+    """
+    Recursively sum all numbers in a nested list structure
+
+    Args:
+    nested_list (list): A list that may contain numbers and/or other lists of numbers
+
+    Returns:
+    int or float: The sum of all numbers in the nested list structure.
+
+    Raises:
+    TypeError: If an element is neight a number not a list.
+    """
+
+    pass
